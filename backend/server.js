6e659b2c0f0266963763fe12b9993ab68c4bbe3e
bayanfuad <bayanjawawdeh9@gmail.com>
2022-11-09 17:14:20 +0200

    solve problems

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,136 @@
+"use strict";
+
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
 app.use(cors());
+app.use(express.json());
 
+const socketIO = require("socket.io");
 const server = http.createServer(app);
-
-const io = new Server(server, {
+const io = socketIO(server, {
+  transports: ["websocket", "polling"],
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
-
-const users = {};
-
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
-
-app.post("/username", (req, res) => {
-  console.log(req.query.username);
-  
-  res.send("Username received");
-});
-
-
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-  
-  socket.on("join_room", (room) => {
-    users[socket.id] = room;
-    console.log(`User ${socket.id} joined the channel ${room}`);
-    socket.join(room);
-    console.log(users)
+  socket.on("connected", () => {
+    console.log(`Socket ${socket.id} disconnected!`);
   });
 
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+  socket.on("joined", (newUser) => {
+    console.log(`${newUser} joined the chat!`);
   });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+  socket.on("left", (name) => {
+    console.log(`${name} left the chat!`);
+  });
+
+  socket.on("disconnect", (name) => {
+    console.log(`${name} disconnected!`);
   });
 });
 
 
 
-server.listen(3001, () => {
-  console.log("SERVER UP AND RUNNING");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
+
+const { User } = require("./models");
+const { db } = require("./models");
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({ where: { id } });
+    res.status(200).json(user);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  try {
+    const user = await User.findAll();
+    res.status(200).json(user);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const data = req.body;
+    const check = await User.findOne({ where: { user: data.user } });
+    if (check) {
+      io.emit("joined", data.user);
+      io.emit("message-added");
+      res.status(200).json(check);
+    } else {
+      const newUser = await User.create(data);
+      io.emit("joined", newUser.user);
+      io.emit("message-added");
+      res.status(200).json(newUser);
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.post("/user/:name", async (req, res) => {
+  try {
+    const name = req.params.name;
+    io.emit("left", name);
+    res.send(`${name} left the chat!`);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  try {
+    const data = req.body;
+    if (data.message === "") {
+      io.emit("message-added");
+      console.log("Empty message");
+      res.status(201).json(users);
+    }
+    const users = await User.findAll();
+    await users.forEach(async (user) => {
+      await user.update({
+        messages: [
+          ...user.messages,
+          `${data.name}:${
+            data.message
+          }:status:unread:date:${Date.now()}:printDate:${new Date(
+            Date.now()
+          ).toLocaleString()}`,
+        ],
+      });
+    });
+
+    io.emit("message-added");
+
+    res.status(201).json(users);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+db.sync()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server up and running on port ${PORT}`);
+    });
+  })
+  .catch(console.error);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,11 @@
-import "./App.css";
-import io from "socket.io-client";
-import { useState } from "react";
-import Chat from "./Chat";
+import './App.css';
+import Chat from './components/main';
 
-var ls = require('local-storage');
-let onlineUsers = [];
-
-const socket = io.connect("http://localhost:3001");
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
-
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-    
-      socket.emit("join_room", room);
-      setShowChat(true);
-    }
-  };
-
   return (
     <div className="App">
-      {!showChat ? (
-        <div className="join-chat-container">
-          <h3>chats</h3>
-          <input
-            type="text"
-            placeholder="Enter your name"
-            onChange={(event) => {
-              setUsername(event.target.value);
-              onlineUsers.push(event.target.value);
-              ls.set('onlineUsers', onlineUsers);
-
-            }}
-          />
-          <select onChange={(e) => {
-            setRoom(e.target.value)
-          }}>
-            <option value="unselected">Select a Channel</option>
-            <option value="general">General</option>
-            <option value="fun">Fun</option>
-            <option value="other">Other</option>
-            
-          </select>
-         
-          <button onClick={joinRoom}>Join This Channel</button>
-        </div>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
+      <Chat />
     </div>
   );
 }
